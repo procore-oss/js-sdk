91157298c871b85a82e1d06df6590f1d5d07cfc8
Paul Nispel <pnispel@gmail.com>
2017-06-22 14:20:21 -0700

    param cant have question mark and initializer

--- lib/info.ts
+++ lib/info.ts
@@ -1,7 +1,7 @@
 import 'isomorphic-fetch'
 import _hostname from './hostname'
 
-function info(token: string, hostname?: string = _hostname): Promise<any> {
+function info(token: string, hostname: string = _hostname): Promise<any> {
   return fetch(`${hostname}/oauth/token/info`, { method: 'POST', headers: { 'Authorization': `Bearer ${token}` } })
     .then((res) => res.json())
 }
