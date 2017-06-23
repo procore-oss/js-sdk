19babcdd51c80feda34691790490c2be62db96f5
Paul Nispel <pnispel@gmail.com>
2017-06-23 10:09:34 -0700

    pass as second argument to authorize

--- lib/authorize.ts
+++ lib/authorize.ts
@@ -3,10 +3,9 @@ import _hostname from './hostname'
 export interface AuthorizeConfig {
   clientId: string;
   uri: string;
-  hostname?: string;
 }
 
-function authorize({ clientId, uri, hostname = _hostname }: AuthorizeConfig): string {
+function authorize({ clientId, uri }: AuthorizeConfig, hostname = _hostname): string {
   return `${hostname}/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${uri}`
 }
 
