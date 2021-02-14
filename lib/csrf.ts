4371ee3bc64e90fbdea27c4066cefdcd439b2c70
Vadim Feshchenko <vadim.feshchenko-contractor@procore.com>
2021-02-14 19:01:52 +0200

    [ZL-796] - Fix issue #41 apply 'getCsrfTokenFromCookie' approach

--- lib/csrf.ts
+++ lib/csrf.ts
@@ -2,12 +2,17 @@ import { Authorizer } from './interfaces'
 
 class Csrf implements Authorizer {
   public authorize(request: Function): Promise<any> {
-    return request(['X-CSRF-TOKEN', window.document.head.querySelector("[name=csrf-token]").getAttribute('content')])
+    return request(['X-CSRF-TOKEN', getTokenFromCookie()])
   }
 }
 
 function csrf(): Csrf {
   return new Csrf()
+}
+
+function getTokenFromCookie() {
+  var token = document.cookie.match('(^|;)\\s*csrf_token\\s*=\\s*([^;]+)');
+  return token ? token.pop() : '';
 }
 
 export default csrf
