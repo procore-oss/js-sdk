f3f805d86e3a4e3009711470594950ef99f89765
Gil McQuillan <gil.mcquillan@procore.com>
2021-02-16 17:56:10 -0600

    Updates to csrf.ts and tests.

--- lib/csrf.ts
+++ lib/csrf.ts
@@ -5,22 +5,26 @@ class Csrf implements Authorizer {
   public csrfHeader: String;
 
   constructor(csrfHeader?: String, getToken?: Function) {
-    this.getToken = getToken || Csrf.getTokenFromCookie
-    this.csrfHeader = csrfHeader || 'X-CSRF-TOKEN'
+    this.getToken = getToken || Csrf.getTokenDefault;
+    this.csrfHeader = csrfHeader || 'X-CSRF-TOKEN';
   }
 
   public authorize(request: Function): Promise<any> {
-    return request([this.csrfHeader, this.getToken()])
+    return request([this.csrfHeader, this.getToken()]);
   }
 
-  private static getTokenFromCookie(): String {
-    var token = document.cookie.match('(^|;)\\s*csrf_token\\s*=\\s*([^;]+)');
-    return token ? decodeURIComponent(token.pop()) : '';
+  // Default location to search for csrf-token is
+  // <head>
+  //   <meta name="csrf-token" content="{{ csrf_token() }}">
+  // </head>
+  // https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html
+  private static getTokenDefault(): String {
+    return window.document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   }
 }
 
 function csrf(csrfHeader?: String, getToken?: Function ): Csrf {
-  return new Csrf(csrfHeader, getToken)
+  return new Csrf(csrfHeader, getToken);
 }
 
-export default csrf
+export default csrf;
--- test/csrf.spec.ts
+++ test/csrf.spec.ts
@@ -5,14 +5,27 @@ import { expect } from 'chai'
 describe('csrf', () => {
 
   describe('#authorize', () => {
-    it('provides token header for the request', () => {
+    it('provides csrf token header with custom getToken function for the request', (done) => {
       const getToken = () => 'fake_token';
       const authorizer = csrf('X-CSRF-TOKEN', getToken);
-      const request = spy()
+      const request = spy();
 
-      authorizer.authorize(request)
+      authorizer.authorize(request);
       
-      expect(request.calledWith(['X-CSRF-TOKEN', 'fake_token'])).to.eql(true)
-    })
-  })
+      expect(request.calledWith(['X-CSRF-TOKEN', 'fake_token'])).to.eql(true);
+      done();
+    });
+
+    it('provides csrf token header with custom csrf header name and getToken function for the request', (done) => {
+      const getToken = () => 'fake_token';
+      const authorizer = csrf('anti-csrf-token', getToken);
+      const request = spy();
+
+      authorizer.authorize(request);
+      
+      expect(request.calledWith(['anti-csrf-token', 'fake_token'])).to.eql(true);
+      done();
+    });
+
+  });
 })
